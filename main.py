9aa677d8600d25089d2bfa653ee6e564a62c9ffb
Samra <118079600+SamraAzizi@users.noreply.github.com>
2024-12-15 21:19:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 import asyncio
 
+from dotenv import load_dotenv
 from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
 from livekit.agents.voice_assistant import VoiceAssistant
 from livekit.plugins import openai, silero
+from api import AssistantFunc
 
-from dotenv import load_dotenv
 
 load_dotenv()
 
 
 async def entrypoint(ctx: JobContext):
-
-    intital_ctx = llm.chat_context().append(
+    initial_ctx = llm.ChatContext().append(
         role="system",
-        text=("you are a voice assistant created by livekit, your interface with user will be voice"
-             "you should short and concise responses and avoid using a unpronoucable pubtualities"
+        text=(
+            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation."
         ),
     )
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+    fnc_ctx = AssistantFnc()
 
-    assistant = VoiceAssistant(
+    assitant = VoiceAssistant(
         vad=silero.VAD.load(),
         stt=openai.STT(),
         llm=openai.LLM(),
         tts=openai.TTS(),
-        chat_ctx=intital_ctx
+        chat_ctx=initial_ctx,
+        fnc_ctx=fnc_ctx,
     )
-
-    assistant.start(ctx.room)
+    assitant.start(ctx.room)
 
     await asyncio.sleep(1)
-
-    await assistant.say("hey! how can I assist you today!", allow_interruptions=True)
-
-
-
+    await assitant.say("Hey, how can I help you today!", allow_interruptions=True)
 
 
 if __name__ == "__main__":
